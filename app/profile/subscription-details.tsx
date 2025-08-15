e6095e3c834c2ecc8c4aa7b822ab9296cdddeee6
Mehmet <mehmeteyupoglu.ads@gmail.com>
2025-08-15 10:49:52 +0300

    added info card to the profile subscription page

--- app/profile/subscription-details.tsx
+++ app/profile/subscription-details.tsx
@@ -1,9 +1,17 @@
-import { Text, View } from "react-native";
+import InfoCard from "@/components/InfoCard";
+import { View } from "react-native";
+import { profileStyles } from "./styles";
 
 export default function SubscriptionDetails() {
   return (
-    <View>
-      <Text>SubscriptionDetails</Text>
+    <View style={profileStyles.container}>
+      <View style={profileStyles.content}>
+        <InfoCard
+          sectionTitle="ABONELİK TARİHLERİ"
+          cardTitle="15 Ağustos - 21 Ağustos"
+          cardDescription="Başlangıç tarihi bugünden itibaren en az 3 gün olmalıdır"
+        />
+      </View>
     </View>
   );
 }
