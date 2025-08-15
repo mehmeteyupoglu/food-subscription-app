c2755a1b00fdcb5e6804e4f4633e0f6e1c92a832
Mehmet <mehmeteyupoglu.ads@gmail.com>
2025-08-15 12:40:45 +0300

    Enhance SubscriptionDetails component by adding ProductCard and SubscriptionStatus for improved subscription overview. Introduce gap styling for better spacing between elements.

--- app/profile/subscription-details.tsx
+++ app/profile/subscription-details.tsx
@@ -1,17 +1,37 @@
 import InfoCard from "@/components/InfoCard";
-import { View } from "react-native";
+import ProductCard from "@/components/ProductCard";
+import SubscriptionStatus from "@/components/SubscriptionStatus";
+import { StyleSheet, View } from "react-native";
 import { profileStyles } from "./styles";
 
 export default function SubscriptionDetails() {
   return (
     <View style={profileStyles.container}>
       <View style={profileStyles.content}>
+
+        <ProductCard
+          image={require("../../assets/manti.png")}
+          subscriptionPlanLabel="Ev Yemekleri Paketi - 5 Gün"
+          discountedMealPrice={310}
+          mealTypeLabel="Öğle Yemekleri"
+          personCount={1}
+          showQuantitySelector={false}
+        />
+        <View style={styles.gap} />
         <InfoCard
           sectionTitle="ABONELİK TARİHLERİ"
           cardTitle="15 Ağustos - 21 Ağustos"
           cardDescription="Başlangıç tarihi bugünden itibaren en az 3 gün olmalıdır"
         />
+        <View style={styles.gap} />
+        <SubscriptionStatus currentStep={3} />
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  gap: {
+    height: 20,
+  },
+});
